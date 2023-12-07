920dfe40b2a069edb26796af04b80711068d3543
Elvis <egannem@gmail.com>
2023-12-07 16:35:50 -0300

    Creating header component

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <nav className="flex items-center bg-gray-100 p-8">
+            <i className="fa fa-bars text-2xl"></i>
+            <h1 className="text-center text-3xl font-bold w-full">BlogX</h1>
+        </nav>
+    );
+}
+
+export default Header;
